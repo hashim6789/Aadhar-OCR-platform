8d9ec09fc49d953c65fed4f2fe2353c8f7d804fa
hashim6789 <muhammedhashim6789@gmail.com>
2025-05-17 10:38:39 +0530

    fix : the dockerfile issue is resolved

--- frontend/src/rough.ts
+++ frontend/src/rough.ts
@@ -1,10 +1,10 @@
-import { IUser } from "./types";
+// import { IUser } from "./types";
 
-export const sampleUser: IUser = {
-  _id: "1",
-  email: "example@gmail.com",
-  name: "user 1",
-  role: "admin",
-  isBlocked: false,
-  isVerified: true,
-};
+// export const sampleUser: IUser = {
+//   _id: "1",
+//   email: "example@gmail.com",
+//   name: "user 1",
+//   role: "admin",
+//   isBlocked: false,
+//   isVerified: true,
+// };
